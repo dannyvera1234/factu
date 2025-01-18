07a901244da135f0ba2313da090672223e18e293
daniel vera <desarrollandoideas8@gmail.com>
2025-01-18 14:29:27 -0500

    feat: actualizar la URL de la API en el entorno de desarrollo

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,10 @@
  export const environment= {
 
    production: true,
-  //  BASE_API_SISTEMA_CONTABLE: "http://localhost:8088/sistemacontable/api/v1",
-  BASE_API_SISTEMA_CONTABLE: "http://93.188.164.46:8089/sistemacontable/api/v1"
+
+  // BASE_API_SISTEMA_CONTABLE: "http://93.188.164.46:8089/sistemacontable/api/v1"
+    BASE_API_SISTEMA_CONTABLE: "http://www.olimpush.com:8089/sistemacontable/api/v1"
 
  }
+
+
