b43375e09a672e77d3f928910676411430f503bb
daniel vera <desarrollandoideas8@gmail.com>
2025-01-21 12:29:55 -0500

    feat: agregar menú de navegación responsivo y funcionalidad de botón hamburguesa en el componente pega-informative

--- src/app/web/pega-informative/pega-informative.component.ts
+++ src/app/web/pega-informative/pega-informative.component.ts
@@ -15,6 +15,12 @@ import { RouterLink } from '@angular/router';
 export class PegaInformativeComponent {
   selectedPlan: string | null = null;
 
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
   // Función para seleccionar un plan
   selectPlan(plan: string) {
     this.selectedPlan = plan;
