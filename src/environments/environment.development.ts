651ae676c3d49e3d4565acc8f0ad4a13833f7e5c
Ronny Chamba <ronnychamba96@gmail.com>
2025-01-25 03:52:00 -0500

    fix path

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,7 +3,7 @@ export const environment = {
   // BASE_API_SISTEMA_CONTABLE: "http://93.188.164.46:8089/sistemacontable/api/v1"
 
   // api de produccion
-  BASE_API_SISTEMA_CONTABLE: 'https://www.olimpush.com:8089/sistemacontable/api/v1',
+  BASE_API_SISTEMA_CONTABLE: 'http://olimpush.com:8089/sistemacontable/api/v1',
 
   // api de desarrollo
   // BASE_API_SISTEMA_CONTABLE: 'http://www.olimpush.com:8083/sistemacontable/api/v1',
