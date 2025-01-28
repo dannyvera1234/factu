bc287affbfad54e34ed2d15faa16b410cd13fcc5
Ronny Chamba <ronnychamba96@gmail.com>
2025-01-27 20:06:26 -0500

    se cambia url de dev a prod

--- src/app/services/service-empresas/inventario.service.ts
+++ src/app/services/service-empresas/inventario.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '@/environments/environment.development';
+import { environment } from '@/environments/environment';
 import { Modulos } from '@/utils/permissions';
 import { HttpService } from '@/utils/services';
 import { PayloadService } from '@/utils/services/payload.service';
