3e358d816b2cc03fc3e9fde6ff7c93406b27f92f
Ronny Chamba <ronnychamba96@gmail.com>
2025-02-04 18:40:25 -0500

    se sube vesion prod, proformas

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   BASE_API_SISTEMA_CONTABLE: 'https://dev.olimpush.com/sistemacontable',
-  version: '1.0.5-test', // Esta es la versión que se usará en el tag de la imagen Docker
+  version: '1.2.0-test', // Esta es la versión que se usará en el tag de la imagen Docker
 };
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   BASE_API_SISTEMA_CONTABLE: 'https://olimpush.com/sistemacontable',
    production: true,
-   version: '1.0.5',
+   version: '1.2.0',
  }
