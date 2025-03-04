3ce38eda892b153b171d5770134ed062f4225b82
daniel vera <desarrollandoideas8@gmail.com>
2025-03-03 19:58:23 -0500

    feat: agregar gráfico utilizando Chart.js en el componente de inicio y optimizar la lógica de inicialización

--- src/app/features-empresas/home/home.component.ts
+++ src/app/features-empresas/home/home.component.ts
@@ -1,8 +1,19 @@
-import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  computed,
+  effect,
+  inject,
+  OnInit,
+  PLATFORM_ID,
+  signal,
+} from '@angular/core';
 import { ConfiguracionService } from '@/services/service-empresas';
 import { GeneriResp } from '@/interfaces';
 import { DetailsService } from '../../feature-counters/details-counter-application';
-import { NgClass } from '@angular/common';
+import { isPlatformBrowser, NgClass } from '@angular/common';
+import { ChartModule } from 'primeng/chart';
 
 interface InfoCardData {
   totalClientes: number;
@@ -14,12 +25,12 @@ interface InfoCardData {
 
 @Component({
   selector: 'app-home',
-  imports: [NgClass],
+  imports: [NgClass, ChartModule],
   templateUrl: './home.component.html',
   styles: ``,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   public readonly infoPlan = signal<GeneriResp<any> | null>(null);
   public readonly card = computed(() => this.detailsService.info());
   public readonly infoCard = signal<GeneriResp<InfoCardData> | null>(null);
@@ -75,6 +86,7 @@ export class HomeComponent {
   constructor(
     private readonly configuracionService: ConfiguracionService,
     private readonly detailsService: DetailsService,
+    private cd: ChangeDetectorRef,
   ) {
     this.detailsHome();
   }
@@ -85,5 +97,84 @@ export class HomeComponent {
         this.infoCard.set(response);
       }
     });
+  }
+
+  data: any;
+
+  options: any;
+
+  platformId = inject(PLATFORM_ID);
+
+  ngOnInit() {
+    this.initChart();
+  }
+
+  initChart() {
+    if (isPlatformBrowser(this.platformId)) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--p-text-color');
+      const textColorSecondary = documentStyle.getPropertyValue('--p-text-muted-color');
+      const surfaceBorder = documentStyle.getPropertyValue('--p-content-border-color');
+
+      this.data = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+          {
+            type: 'line',
+            label: 'Dataset 1',
+            borderColor: documentStyle.getPropertyValue('--p-orange-500'),
+            borderWidth: 2,
+            fill: false,
+            tension: 0.4,
+            data: [50, 25, 12, 48, 56, 76, 42],
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 2',
+            backgroundColor: documentStyle.getPropertyValue('--p-gray-500'),
+            data: [21, 84, 24, 75, 37, 65, 34],
+            borderColor: 'white',
+            borderWidth: 2,
+          },
+          {
+            type: 'bar',
+            label: 'Dataset 3',
+            backgroundColor: documentStyle.getPropertyValue('--p-cyan-500'),
+            data: [41, 52, 24, 74, 23, 21, 32],
+          },
+        ],
+      };
+
+      this.options = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.6,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+      this.cd.markForCheck();
+    }
   }
 }
